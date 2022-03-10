35e183610e342b954af0137704de2a9edcb46875
ro-ken <56027589+ro-ken@users.noreply.github.com>
2022-03-10 20:45:23 +0800

    add boots.py

--- model/style_transfer/settings.py
+++ model/style_transfer/settings.py
@@ -8,16 +8,18 @@
 # 内容特征层及loss加权系数
 import os
 
+ROOT = os.path.split(os.path.realpath(__file__))[0] + '/'
+
 CONTENT_LAYERS = {'block4_conv2': 0.5, 'block5_conv2': 0.5}
 # 风格特征层及loss加权系数
 STYLE_LAYERS = {'block1_conv1': 0.2, 'block2_conv1': 0.2, 'block3_conv1': 0.2, 'block4_conv1': 0.2,
                 'block5_conv1': 0.2}
 # 内容图片路径
-CONTENT_IMAGE_PATH = './images/content.jpg'
+CONTENT_IMAGE_PATH = ROOT + 'images/content.jpg'
 # 风格图片路径
-STYLE_IMAGE_PATH = './images/style.jpg'
+STYLE_IMAGE_PATH = ROOT + 'images/style.jpg'
 # 生成图片的保存目录
-OUTPUT_DIR = os.path.split(os.path.realpath(__file__))[0] + '/output'
+OUTPUT_DIR = ROOT + 'output'
 
 # 内容loss总加权系数
 CONTENT_LOSS_FACTOR = 1
