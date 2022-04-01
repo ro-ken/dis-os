84a98be911a7f0c820576da232c96629ebc689a6
huomax <630509357@qq.com>
2022-04-01 19:32:23 +0800

    mod a error

--- Test/Test.py
+++ Test/Test.py
@@ -7,8 +7,16 @@ def API_RandomTask_MaxLoad(task_number, start_label, end_label, max_times, max_l
 
 # 随机生成测试任务
 def API_RandomTask_LoadDict(task_number, start_label, end_label, max_times, load_dict):
-    task_list = TaskTest._random_task_loaddict(task_number, start_label, end_label, max_times, load_dict)
+    task_list = TaskTest.RandomTask_LoadList(task_number, start_label, end_label, max_times, load_dict)
     if task_list == None:
         print("Error: 参数不合法")
         return []
     return task_list
+
+
+# if __name__ == '__main__':
+#     task_list = API_RandomTask_MaxLoad(10, 0, 6, 3, 10)
+#     print(task_list)
+#     a = {1:2,2:4,3:4,4:5}
+#     task_list = API_RandomTask_LoadDict(10, 1, 4,3, a)
+#     print(task_list)
