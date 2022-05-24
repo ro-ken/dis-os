836feebe03ba522e75338e8cf50c9c6e5b7c1f51
ro-laptop <rq8866@126.com>
2022-05-24 20:20:38 +0800

    update node coef

--- exper/linear_regression/main.py
+++ exper/linear_regression/main.py
@@ -12,7 +12,7 @@ def run():
         multilist = [[0 for col in range(row_len)] for row in range(size)]
         #print(type(multilist))
         i=str(i*10)
-        f = open("data/smp/local_task_time_cpu_"+i+".txt",encoding="utf-8")
+        f = open("data/smp_old/local_task_time_cpu_"+i+".txt",encoding="utf-8")
         for line in f.readlines():
             trash, time = line.split(':')
             j,k = time.split('\n')
@@ -28,7 +28,7 @@ def run():
         #print(f.readlines())
         f.close()
     # for i in range(11):
-    #     for j in range(12):
+    #     for j in range(row_len):
     #         print(multilist[i][j])
     #     print('\n')
 
@@ -36,14 +36,23 @@ def run():
     for j in range(6):
         num = []
         num1 = []
-        for i in range(11):
-            temp = float(multilist[i][j*2+1])-float(multilist[i][j*2])
-            num.append(float(temp))
-            num1.append(int(i+1))
+        for i in range(10):
+
+            tmp = float(multilist[i][j*2+1])-float(multilist[i][j*2])
+            # n = 3
+            # temp1 = float(multilist[i][j*2+1])-float(multilist[i][j*2])
+            # temp2 = float(multilist[i][12+j*2+1])-float(multilist[i][12+j*2])
+            # temp3 = float(multilist[i][24+j*2+1])-float(multilist[i][24+j*2])
+            # tmp = (temp1 + temp2 + temp3 )/3
+            # if temp3 <0:
+            #     tmp = (temp1 + temp2)/2
+            # print("temp1 = {} ,temp2 = {} , temp3 = {}".format(temp1,temp2,temp3))
+            num.append(float(tmp))
+            num1.append(int(i)*10)
     #plt.figure(figsize=10)
         plt.title('pic')
-        plt.xlabel('turn')
-        plt.ylabel('time')
+        plt.xlabel('cpu ratio %')
+        plt.ylabel('time/s')
         plt.plot(num1,num)
         x=np.array(num1).reshape(-1,1)
         y=np.array(num)
@@ -53,7 +62,7 @@ def run():
 def linear(x,y):
     model = linear_model.LinearRegression()
     model.fit(x,y)
-    j = np.arange(0,12,1)
+    j = np.arange(0,100,1)
     k = model.coef_*j+model.intercept_
     plt.plot(j,k)
     print("a = "+str(model.coef_)+" b="+str(model.intercept_))
