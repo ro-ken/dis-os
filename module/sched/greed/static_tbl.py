28fe77bb6d96df4e5afd9204c24b5376c14f4d98
ro_xx_win11 <rq8866@126.com>
2022-07-12 15:57:52 +0800

    add more vma config

--- module/sched/greed/static_tbl.py
+++ module/sched/greed/static_tbl.py
@@ -1,4 +1,5 @@
 import settings
+
 # 任务序号 0,1,2...
 
 task_time_smp = (2.53, 0.46, 0.01, 41.54, 7.75, 15.63, 1000000)
@@ -8,7 +9,7 @@ task_time_win = (0.33, 0.23, 0.01, 3.45, 1.34, 2.20, 2.95)
 
 # 静态时间表
 task_time_table = {"smp": task_time_smp, "ywd": task_time_ywd, "hwj": task_time_hwj, "win": task_time_win,
-                   "vma": task_time_smp,"loc":task_time_smp}
+                   "vma": task_time_smp, "loc": task_time_smp}
 
 # 任务运行时间随CPU负载变化的系数 y=ax+b 系数(a,b)
 vedio_coef_hwj = (-1.7288 * 10 ** (-8), 3.3566 * 10 ** (-6), -1.5542 * 10 ** (-4), 0.0069, 0.710699)
@@ -30,7 +31,8 @@ coef_hwj = (
 
 # 比例系数表
 task_coef_table = {"smp": coef_smp, "hwj": coef_hwj, "win": coef_hwj, "smp2": coef_smp2, "smp3": coef_smp3,
-                   "vma": coef_smp, "loc":coef_smp}
+                   "vma": coef_smp, "loc": coef_smp, "vma2": coef_smp, "vma3": coef_smp, "vma4": coef_smp,
+                   "vma5": coef_smp, "vma6": coef_smp}
 
 '''
     以下为传输时延
@@ -41,14 +43,17 @@ default_diff_node_delay = 1.0
 
 # 本节点测量延迟
 node_self_trans_delay = {"smp": 0, "hwj": 0, "win": 0, "smp2": 0, "smp3": 0,
-                   "vma": 0, "local":0}
+                         "vma": 0, "local": 0, "vma2": 0, "vma3": 0, "vma4": 0, "vma5": 0, "vma6": 0}
 
 # 未测量延迟
 unscanned_delay = {"smp": 0, "hwj": 0, "win": 0, "smp2": 0, "smp3": 0,
-                   "vma": 0, "local":0}
+                   "vma": 0, "local": 0, "vma2": 0, "vma3": 0, "vma4": 0, "vma5": 0, "vma6": 0, }
 
-diff_node_delay_table = {"smp": unscanned_delay, "hwj": unscanned_delay, "win": unscanned_delay, "smp2": unscanned_delay, "smp3": unscanned_delay,
-                   "vma": unscanned_delay, "local":unscanned_delay}
+diff_node_delay_table = {"smp": unscanned_delay, "hwj": unscanned_delay, "win": unscanned_delay,
+                         "smp2": unscanned_delay, "smp3": unscanned_delay,
+                         "vma": unscanned_delay, "local": unscanned_delay, "vma2": unscanned_delay,
+                         "vma3": unscanned_delay, "vma4": unscanned_delay, "vma5": unscanned_delay,
+                         "vma6": unscanned_delay}
 
 '''
     以下为向上层暴露的api函数
@@ -60,6 +65,7 @@ def task_time_table_fun(name):
     arch = name[:3]  # 取前3个作为key：例 name = smp2 ，arch = smp
     return task_time_table[arch]
 
+
 # 返回任务处理时间系数表
 def task_coef_table_fun(name):
     arch = name[:3]  # 取前3个作为key：例 name = smp2 ，arch = smp
@@ -67,6 +73,7 @@ def task_coef_table_fun(name):
         return task_coef_table[name]
     else:
         return task_coef_table[arch]
+
 
 # 返回传输时延
 def node_trans_delay_table_fun(name):
